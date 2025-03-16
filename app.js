fe5e5aea5c35155ccb50271ef2ebd9e9ca949a9f
anamurim <ana.amurim@gmail.com>
2025-03-16 15:39:58 -0300

    Corregir Adicionar amigo

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 let amigos = [];
 //let listaDeAmigos = [];
-//let quantidadeDeAmigos = 0;
+//let quantidadeDeAmigos = amigos.length; //Conta quantos itens tem em amigos
 let amigoSecreto = gerarAmigoAleatorio();
 
 //Função para adicionar amigos
 function adicionarAmigo(){
-    let amigo = document.querySelector('input').value;
+    const adicionaAmigo = document.getElementById('amigo').value;
+    const novoAmigo = adicionaAmigo;
 
-    if(amigo){
-        //quantidadeDeAmigos =+ 1;
+
+    if(adicionaAmigo == ""){
+        alert('Por favor, insira um nome!');
+    }else{
+        //quantidadeDeAmigos =+ 1
         //listaDeAmigos = amigo;
         
-        amigos.push(amigo);
+        amigos.push(novoAmigo);
         alert(`${amigos} adicionado à lista`);
-        amigos.innerHTML = "";
-
-    }else{
-        alert('Por favor, insira um nome!');
+        //adicionaAmigo = "";
     }
+
+     
+        
+
 }
 
+//Atualizar lista de amigos
 function atulizarListaDeAmigos(){
-    
+    const listaAmigos = document.getElementById(listaAmigos);
+
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        const lista = document.createElement('li');
+        lista.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
     
 }
 
