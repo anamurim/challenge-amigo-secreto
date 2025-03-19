3f7f6c11b0f54910985d96eee796dcce3edbe1e9
anamurim <ana.amurim@gmail.com>
2025-03-18 23:44:18 -0300

    Sortear amigo

--- app.js
+++ app.js
@@ -1,52 +1,89 @@
-let amigos = [];
+let nomesDosAmigos = [];
 
-//Função para adicionar amigos
+let campoTexto = document.getElementById("amigo");
+
+
+
+campoTexto.value = "";
+
+
+
+
+
 function adicionarAmigo(){
-    const adicionaAmigo = document.getElementById('amigo');
-    const novoAmigo = adicionaAmigo.value.trim();
 
-    if(novoAmigo == ""){
-        alert('Por favor, insira um nome!');
-        return;
+    if(campoTexto.value == ""){
+
+        alert("Por favor, insira um nome");
+
+    } else if(nomesDosAmigos.includes(campoTexto.value)) {
+
+        alert(`${campoTexto.value} já está na lista`);
+
+        campoTexto.value = "";
+
+    } else{
+
+        nomesDosAmigos.push(campoTexto.value);
+
+        campoTexto.value = "";
+
     }
-    
-    if(amigos == novoAmigo){
-        alert(`${novoAmigo} já está na lista.`);
-        adicionaAmigo.value = "";
-        return;
-    }
-    
-    amigos.push(novoAmigo);
-    ///alert(`${novoAmigo} adicionado à lista.`);
-       
-    adicionaAmigo.value = "";
+
+
+
     atualizarLista();
+
 }
 
-//Atualizar lista de amigos
-function atulizarLista(){
-    const listaAmigos = document.getElementById('listaAmigos');
 
-    listaAmigos.innerHTML = "";
 
-    for(let i = 0; i < amigos.length; i++){
-        const li = document.createElement('li');
-        li.textContent = amigos[i];
-        listaAmigos.appendChild(li);
+function atualizarLista(){
+
+    let campoLista = document.getElementById("listaAmigos");
+
+    campoLista.innerHTML = "";
+
+
+
+    for(let i = 0; i < nomesDosAmigos.length; i++){
+
+        let li = document.createElement("li");
+
+        li.textContent = nomesDosAmigos[i];
+
+        campoLista.appendChild(li);
+
     }
+
 }
+
+
 
 function sortearAmigo(){
-    if(amigos.length == 0){
+
+    if(nomesDosAmigos.length == 0){
+
         alert("Não há nomes cadastrados!");
+
         return;
+
     }
 
-    const indiceAmigo = parseInt(Math.random() * amigos.length);
 
-    const amigoSecreto = amigos[indiceAmigo];
 
-    const resultado = document.getElementById('resultado');
+    let indiceAmigo = parseInt(Math.random() * nomesDosAmigos.length);
+
+
+
+    let amigoSecreto = nomesDosAmigos[indiceAmigo];
+
+
+
+    let resultado = document.getElementById('resultado');
+
+
 
     resultado.innerHTML = `Amigo sorteado: <strong>${amigoSecreto}</strong>`;
+
 }
